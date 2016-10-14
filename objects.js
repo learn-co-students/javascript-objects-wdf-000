c0b6d489118936dbd021cd1f515b8f5f9d4ce828
ozPop <ozziepopovas@gmail.com>
2016-10-14 15:55:04 -0400

    Done.

--- objects.js
+++ objects.js
@@ -2,6 +2,10 @@ var playlist = new Object({ artistName: 'songName'});
 
 function updatePlaylist(obj, artistName, songTitle) {
   return Object.assign({}, obj, {[artistName]: songTitle});
+  
+  // Solution
+  // playlist[artist] = song
+  // return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
