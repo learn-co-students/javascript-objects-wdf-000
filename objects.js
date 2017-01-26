d8b341028bba28dfca7df5ce2ab44773be30b55f
Ekaterina Petushova <epetushova@gmail.com>
2017-01-26 13:17:38 -0500

    Done.

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artistName: "songTitle"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-	playlist[artistName] = songTitle
+	playlist[artistName] = "songTitle";
 }
 
 function removeFromPlaylist(playlist, artistName){
